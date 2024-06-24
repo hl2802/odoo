148bd730c99feb64adaf754ea6f1b14b0ff4399b
NguyenHuongIT <huongl28022003@gmail.com>
2024-06-24 16:42:08 +0700

    đổi màu của odoo

---
+++ addons/custom_hr_employee/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Custom HR Employee',
+    'version': '1.0',
+    'summary': 'Add custom fields to HR Employee module',
+    'description': 'This module adds custom fields to the HR Employee module.',
+    'author': 'NguyenHuongIT',
+    'category': 'Human Resources',
+    'depends': ['hr'],
+    'data': [
+        'views/hr_employee_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
